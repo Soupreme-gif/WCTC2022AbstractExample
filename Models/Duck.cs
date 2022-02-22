b9c808596d2337d84efc31c1d5a356735a625b43
Soupreme-gif <77864432+Soupreme-gif@users.noreply.github.com>
2022-02-21 20:49:16 -0600

    Add files via upload

---
+++ Models/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    public class Duck : Animal
+    {
+
+        public Duck(string name)
+        {
+            Name = name;
+        }
+
+        public Duck()
+        {
+            Name = "Gerald";
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says Quak Quak");
+        }
+    }
+}
